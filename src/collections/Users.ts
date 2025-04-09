53106a8f88dea1ecb08d75278cf3ca0befc6249e
TokyPNL <ramahefarisontoky@gmail.com>
2025-04-09 16:09:22 +0300

    token expiracy to 7 jours

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,10 +2,12 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  auth: {
+    tokenExpiration: 604800,
+  },
   admin: {
     useAsTitle: 'email',
   },
-  auth: true,
   fields: [
     // Email added by default
     // Add more fields as needed
